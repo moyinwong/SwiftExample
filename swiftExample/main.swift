6b3235016433a9bed4d5d25d22ad6db665735b91
Kelvin Wong <cheukyinwong1995@gmail.com>
2020-10-27 15:25:29 +0800

    swift practice

--- swiftExample/main.swift
+++ swiftExample/main.swift
@@ -9,3 +9,33 @@ import Foundation
 
 print("Hello, World!")
 
+class Track {
+    let name: String
+    let instructor: String
+    
+    init(name: String, instructor: String) {
+        self.name = name
+        self.instructor = instructor
+    }
+}
+
+let tracks = [
+    Track(name: "Web", instructor: "Kelvin"),
+    Track(name: "Mobile", instructor: "Stanley"),
+    Track(name: "Game", instructor: "Clint")
+]
+
+let students = ["Harry", "Scott", "Garvin"]
+
+var assignment: [String : Track] = [:]
+
+for student in students {
+    let track = Int.random(in: 0 ..< tracks.count)
+    assignment[student] = tracks[track]
+}
+
+for (student, track) in assignment {
+    print("\(student) got \(track.name) with \(track.instructor)")
+}
+
+
